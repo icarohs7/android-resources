06583a59f420ee3e1e9a8a1b959b492e92da28b9
Icaro Temponi <icarohs7@gmail.com>
2019-07-25 17:16:53 -0300

    Add awaitDismiss and showAndAwaitDismiss extensions to Dialog
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- dialogresources/src/main/kotlin/base/dialogresources/domain/DialogExtensions.kt
+++ dialogresources/src/main/kotlin/base/dialogresources/domain/DialogExtensions.kt
@@ -1,6 +1,9 @@
 package base.dialogresources.domain
 
 import android.app.Dialog
+import base.dialogresources.presentation.dialogs.BaseMaterialDialog
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 inline fun <D : Dialog, T> D.showWhileRunning(operation: D.() -> T): T {
     return try {
@@ -8,5 +11,20 @@ inline fun <D : Dialog, T> D.showWhileRunning(operation: D.() -> T): T {
         operation()
     } finally {
         dismiss()
+    }
+}
+
+suspend inline fun BaseMaterialDialog<*>.awaitDismiss(): Unit = dialog.awaitDismiss()
+suspend fun <D : Dialog> D.awaitDismiss() {
+    suspendCoroutine<Unit> { cont ->
+        setOnDismissListener { cont.resume(Unit) }
+    }
+}
+
+suspend inline fun BaseMaterialDialog<*>.showAndAwaitDismiss(): Unit = dialog.showAndAwaitDismiss()
+suspend fun <D : Dialog> D.showAndAwaitDismiss() {
+    suspendCoroutine<Unit> { cont ->
+        setOnDismissListener { cont.resume(Unit) }
+        show()
     }
 }
