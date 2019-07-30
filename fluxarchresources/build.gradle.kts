c2dac83ac477b1e17d5807fefbf65b3ba8197de9
Icaro Temponi <icarohs7@gmail.com>
2019-07-30 10:41:36 -0300

    Add fluxarchresources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ fluxarchresources/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlinx-serialization")
+    id("androidx.navigation.safeargs.kotlin")
+    defaults.`android-module`
+}
+
+android {
+    defaultSettings(project)
+}
+
+dependencies {
+    api(project(":corelibrary"))
+
+    api(AndroidDeps.quantum)
+//    implementation(AndroidDeps.quantumRx)
+
+    AndroidKaptDeps.core.forEach(::kapt)
+}
