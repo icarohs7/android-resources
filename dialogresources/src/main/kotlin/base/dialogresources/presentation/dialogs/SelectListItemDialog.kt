f943c2dcfc52d7609225d2672723287701fab9a2
Icaro Temponi <icarohs7@gmail.com>
2019-08-13 08:36:17 -0300

    Add SelectListItemDialog
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ dialogresources/src/main/kotlin/base/dialogresources/presentation/dialogs/SelectListItemDialog.kt
@@ -0,0 +1,19 @@
+package base.dialogresources.presentation.dialogs
+
+import android.content.Context
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.list.SingleChoiceListener
+import com.afollestad.materialdialogs.list.listItemsSingleChoice
+
+@Suppress("FunctionName")
+fun SelectListItemDialog(
+        ctx: Context,
+        title: String = "",
+        items: List<String>,
+        onItemSelect: SingleChoiceListener
+): MaterialDialog {
+    return MaterialDialog(ctx).apply {
+        title(text = title)
+        listItemsSingleChoice(items = items, selection = onItemSelect)
+    }
+}
