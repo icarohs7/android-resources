d4f6d980656d0735f14a98b7f21921fc72577027
Icaro Temponi <icarohs7@gmail.com>
2019-08-27 15:02:53 -0300

    Setup corelibrary log on Firebase init function
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- firebasecoreresources/src/main/kotlin/base/firebasecoreresources/FirebaseRes.kt
+++ firebasecoreresources/src/main/kotlin/base/firebasecoreresources/FirebaseRes.kt
@@ -2,6 +2,7 @@ package base.firebasecoreresources
 
 import android.app.Activity
 import androidx.core.os.bundleOf
+import base.corelibrary.Corelibrary
 import com.github.icarohs7.unoxcore.UnoxCore
 import com.google.firebase.analytics.FirebaseAnalytics
 import timber.log.Timber
@@ -12,7 +13,13 @@ object FirebaseRes {
         UnoxCore.logger = {
             Timber.tag("UnoxCore").i("$it")
             firebaseAnalytics.logEvent("UnoxCore", bundleOf(
-                    "message" to "$it"
+                    "message" to "$it".take(99)
+            ))
+        }
+        Corelibrary.log = { tag, msg ->
+            Timber.tag(tag).i("$msg")
+            firebaseAnalytics.logEvent(tag, bundleOf(
+                    "message" to "$msg".take(99)
             ))
         }
     }
