9a3609706d197b54fd848aa69f4ed6dc0adea66e
Icaro Temponi <icarohs7@gmail.com>
2019-09-05 11:39:07 -0300

    Add showAlert extension of Context
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ dialogresources/src/main/kotlin/base/dialogresources/presentation/dialogs/SimpleAlert.kt
@@ -0,0 +1,20 @@
+package base.dialogresources.presentation.dialogs
+
+import android.content.Context
+import com.afollestad.materialdialogs.MaterialDialog
+
+fun Context.showAlert(title: String? = null, message: String? = null): MaterialDialog {
+    return MaterialDialog(this).show {
+        title(text = title)
+        message(text = message)
+        positiveButton { }
+    }
+}
+
+fun Context.showAlert(titleRes: Int? = null, messageRes: Int? = null): MaterialDialog {
+    return MaterialDialog(this).show {
+        title(titleRes)
+        message(messageRes)
+        positiveButton { }
+    }
+}
