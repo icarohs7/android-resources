257874bd3b4ec05005d1b648ea523f0429ac8d84
Icaro Temponi <icarohs7@gmail.com>
2019-09-08 22:49:56 -0300

    Add withMapAsync extension of BaseMapFragment
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- googlemapsresources/src/main/kotlin/base/googlemapsresources/domain/extensions/BaseMapFragmentExtensions.kt
+++ googlemapsresources/src/main/kotlin/base/googlemapsresources/domain/extensions/BaseMapFragmentExtensions.kt
@@ -2,6 +2,9 @@ package base.googlemapsresources.domain.extensions
 
 import base.googlemapsresources.presentation.fragments.BaseMapFragment
 import com.google.android.gms.maps.GoogleMap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 suspend fun BaseMapFragment.awaitMap(): GoogleMap {
     return mapView.awaitMap()
@@ -9,4 +12,8 @@ suspend fun BaseMapFragment.awaitMap(): GoogleMap {
 
 fun BaseMapFragment.withMap(callback: GoogleMap.() -> Unit) {
     mapView.getMapAsync(callback)
+}
+
+fun BaseMapFragment.withMapAsync(scope: CoroutineScope, callback: suspend GoogleMap.() -> Unit): Job {
+    return scope.launch { with(awaitMap()) { callback() } }
 }
