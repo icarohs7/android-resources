194e8e5975078eea143cb392edc16f7b815a84a1
Icaro Temponi <icarohs7@gmail.com>
2019-09-10 17:09:53 -0300

    Merge unox-android-arch-spinner into spinnerresources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ spinnerresources/src/main/kotlin/base/spinnerresources/MaterialSpinnerExtensions.kt
@@ -0,0 +1,24 @@
+package base.spinnerresources
+
+import com.jaredrummler.materialspinner.MaterialSpinner
+
+/**
+ * Return the currently selected item
+ * on the spinner
+ */
+fun <T> MaterialSpinner.selectedItem(): T {
+    val index = this.selectedIndex
+    return this.getItems<T>()[index]
+}
+
+/**
+ * Select the given item on the spinner
+ */
+fun <T> MaterialSpinner.selectItem(item: T) {
+    getItems<T>().forEachIndexed { index, i ->
+        if (i == item) {
+            selectedIndex = index
+            return@forEachIndexed
+        }
+    }
+}
