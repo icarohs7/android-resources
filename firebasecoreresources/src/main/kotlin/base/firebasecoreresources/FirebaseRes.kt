ea16651f0124b0892168630de19ddc9eea1bb689
Icaro Temponi <icarohs7@gmail.com>
2019-09-14 18:48:41 -0300

    Remove init member function of FirebaseRes, add getAnalytics overloads
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- firebasecoreresources/src/main/kotlin/base/firebasecoreresources/FirebaseRes.kt
+++ firebasecoreresources/src/main/kotlin/base/firebasecoreresources/FirebaseRes.kt
@@ -4,26 +4,18 @@ import android.app.Activity
 import androidx.core.os.bundleOf
 import base.corextresources.Corelibrary
 import base.coreresources.UnoxAndroidArch
+import base.coreresources.toplevel.onActivity
 import com.github.icarohs7.unoxcore.UnoxCore
 import com.google.firebase.analytics.FirebaseAnalytics
 import timber.log.Timber
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 object FirebaseRes {
-    fun init(activity: Activity) {
-        val firebaseAnalytics = FirebaseAnalytics.getInstance(activity)
-        UnoxCore.logger = {
-            Timber.tag("UnoxCore").i("$it")
-            firebaseAnalytics.logEvent("UnoxCore", bundleOf(
-                    "message" to "$it".take(99),
-                    "debug" to "${UnoxAndroidArch.isDebug}"
-            ))
-        }
-        Corelibrary.log = { tag, msg ->
-            Timber.tag(tag).i("$msg")
-            firebaseAnalytics.logEvent(tag, bundleOf(
-                    "message" to "$msg".take(99),
-                    "debug" to "${UnoxAndroidArch.isDebug}"
-            ))
-        }
+    fun getAnalytics(act: Activity): FirebaseAnalytics = FirebaseAnalytics.getInstance(act)
+    suspend fun getAnalytics(): FirebaseAnalytics {
+        return FirebaseAnalytics.getInstance(suspendCoroutine<Activity> { cont ->
+            onActivity { cont.resume(this) }
+        })
     }
 }
