a67159c4c1543dafb324455b5d027dc17ee39422
Icaro Temponi <icarohs7@gmail.com>
2019-09-16 10:36:30 -0300

    Change default bg and title color on BaseFullscreenMaterialDialog
    Add onCreateView member function to BaseFullscreenMaterialDialog
    
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- dialogresources/src/main/kotlin/base/dialogresources/presentation/dialogs/BaseFullscreenMaterialDialog.kt
+++ dialogresources/src/main/kotlin/base/dialogresources/presentation/dialogs/BaseFullscreenMaterialDialog.kt
@@ -1,8 +1,10 @@
 package base.dialogresources.presentation.dialogs
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import base.dialogresources.R
+import base.dialogresources.domain.toolbar
 import com.github.icarohs7.unoxcore.extensions.coroutines.job
 import com.nikialeksey.fullscreendialog.Dialog
 import com.nikialeksey.fullscreendialog.DismissOnCloseDialog
@@ -17,15 +19,23 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
+import splitties.resources.appColor
 import splitties.resources.appDrawable
+import splitties.views.backgroundColor
 
 abstract class BaseFullscreenMaterialDialog(protected val context: Context, protected val title: String) {
     val lifecycleScope: CoroutineScope = MainScope()
     val dialog by lazy {
-        createDialog(createToolbar(), createDialogContent()).apply {
+        val toolbar = createToolbar()
+        val content = createDialogContent()
+        createDialog(toolbar, content).apply {
             if (this is DismissOnCloseDialog) addOnClose { lifecycleScope.job.cancelChildren() }
             else addOnAction { lifecycleScope.job.cancelChildren() }
+            onCreateView(toolbar, content)
         }
+    }
+
+    open fun onCreateView(toolbar: FsDialogToolbar, view: View) {
     }
 
     open fun createDialog(toolbar: FsDialogToolbar, content: View): Dialog {
@@ -43,7 +53,10 @@ abstract class BaseFullscreenMaterialDialog(protected val context: Context, prot
                 title,
                 FsCloseButton(SimpleButton(), appDrawable(R.drawable.fs_close_icon)!!),
                 FsActionButton(SimpleButton(), "")
-        )
+        ).apply {
+            backgroundColor = appColor(R.color.colorPrimaryVariant)
+            toolbar.setTitleTextColor(appColor(R.color.colorOnPrimary))
+        }
     }
 
     fun show() {
