531690153de1287289d29ebfdeb3cd0f8c6dad8a
Icaro Temponi <icarohs7@gmail.com>
2019-09-28 21:47:22 -0300

    Add awaitDataThrowing extension of ApolloCall
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- graphqlresources/src/main/kotlin/base/graphqlresources/domain/extensions/ApolloExtensions.kt
+++ graphqlresources/src/main/kotlin/base/graphqlresources/domain/extensions/ApolloExtensions.kt
@@ -6,3 +6,11 @@ import com.apollographql.apollo.coroutines.toDeferred
 
 suspend fun <T> ApolloCall<T>.await(): Response<T> = toDeferred().await()
 suspend fun <T> ApolloCall<T>.awaitData(): T? = toDeferred().await().data()
+suspend fun <T> ApolloCall<T>.awaitDataThrowing(): T = with(await()) {
+    if (hasErrors()) {
+        val aggregatedErrors = errors().joinToString("\n") { it.message() ?: "${it.locations()}" }
+        error(aggregatedErrors)
+    }
+
+    data()!!
+}
