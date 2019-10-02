238f5d105b7e791ac89729d8c64ebb12e87549b9
Icaro Temponi <icarohs7@gmail.com>
2019-10-02 10:22:26 -0300

    Add BaseDataAdapterHolder interface with helper methods to convert data
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ corextresources/src/main/kotlin/base/corextresources/data/api/BaseDataAdapterHolder.kt
@@ -0,0 +1,28 @@
+package base.corextresources.data.api
+
+import base.coreresources.extensions.asDate
+import base.coreresources.extensions.asRemoteDate
+import com.github.icarohs7.unoxcore.extensions.orZero
+
+interface BaseDataAdapterHolder {
+    fun int(s: String): Int = s.toIntOrNull().orZero()
+    fun double(s: String): Double = s.toDoubleOrNull().orZero()
+    fun dateStr(s: String, format: String = "dd/MM/yyyy HH:mm"): String = s.asDate(format).asRemoteDate
+
+    /**
+     * Strip html tags from string,
+     * also replace `null` values
+     * with empty strings
+     */
+    fun String?.removeHtmlAndNull(): String {
+        return this
+                .orEmpty()
+                .replace("&lt;", "<")
+                .replace("&gt;", ">")
+                .replace("<br>", "\n")
+                .replace("<br/>", "\n")
+                .replace("<br />", "\n")
+                .replace(Regex("<[^>]*>"), "")
+                .replace(Regex("^null$"), "")
+    }
+}
