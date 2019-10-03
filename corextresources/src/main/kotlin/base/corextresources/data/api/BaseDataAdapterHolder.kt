5c6e1f65d6c407f20f754e8f71aabf6ef78b99ec
Icaro Temponi <icarohs7@gmail.com>
2019-10-03 14:43:47 -0300

    Add boolean to int conversion function to BaseDataAdapterHolder
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- corextresources/src/main/kotlin/base/corextresources/data/api/BaseDataAdapterHolder.kt
+++ corextresources/src/main/kotlin/base/corextresources/data/api/BaseDataAdapterHolder.kt
@@ -6,6 +6,7 @@ import com.github.icarohs7.unoxcore.extensions.orZero
 
 interface BaseDataAdapterHolder {
     fun int(s: String): Int = s.toIntOrNull().orZero()
+    fun int(b: Boolean): Int = if (b) 1 else 0
     fun double(s: String): Double = s.toDoubleOrNull().orZero()
     fun dateStr(s: String, format: String = "dd/MM/yyyy HH:mm"): String = s.asDate(format).asRemoteDate
 
