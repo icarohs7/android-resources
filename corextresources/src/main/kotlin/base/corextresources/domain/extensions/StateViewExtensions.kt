8fc3be6ef119ffee4d07e8ee3c6c618ca0caa42c
Icaro Temponi <icarohs7@gmail.com>
2019-10-09 10:44:10 -0300

    Change StateView extensions
    Remove displayStateWhenListIsEmpty extension
    
    Add displayStateWhenFlowListIsEmpty extension
    
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- corextresources/src/main/kotlin/base/corextresources/domain/extensions/StateViewExtensions.kt
+++ corextresources/src/main/kotlin/base/corextresources/domain/extensions/StateViewExtensions.kt
@@ -1,8 +1,26 @@
 package base.corextresources.domain.extensions
 
+import com.github.icarohs7.unoxcore.UnoxCore
+import com.github.icarohs7.unoxcore.extensions.coroutines.flowOnBackground
 import com.umutbey.stateviews.StateView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
-fun StateView.displayStateWhenListIsEmpty(list: List<*>, stateTag: String) {
-    if (list.isEmpty()) displayState(stateTag)
-    else hideStates()
+/**
+ * @param stateTag Tag of the state to be shown when the given
+ * flow emits an empty list
+ */
+fun StateView.displayStateWhenFlowListIsEmpty(scope: CoroutineScope, flow: Flow<List<*>>, stateTag: String): Job {
+    return flow.map { it.isNotEmpty() }
+            .distinctUntilChanged()
+            .flowOnBackground()
+            .onEach { showContent -> if (showContent) hideStates() else displayState(stateTag) }
+            .flowOn(UnoxCore.foregroundDispatcher)
+            .launchIn(scope)
 }
