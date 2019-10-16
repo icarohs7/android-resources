b862d173b661a4d45f3d266b254b7e02edb6e8ea
Icaro Temponi <icarohs7@gmail.com>
2019-10-15 21:34:48 -0300

    Add toAsync extension of Try
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- corextresources/src/main/kotlin/base/corextresources/domain/extensions/ArrowExtensions.kt
+++ corextresources/src/main/kotlin/base/corextresources/domain/extensions/ArrowExtensions.kt
@@ -3,6 +3,8 @@ package base.corextresources.domain.extensions
 import arrow.core.Failure
 import arrow.core.Success
 import arrow.core.Try
+import com.airbnb.mvrx.Async
+import com.airbnb.mvrx.Fail
 import com.github.icarohs7.unoxcore.extensions.orThrow
 
 /**
@@ -31,4 +33,15 @@ class TryWrapper<T>(val t: Try<T>)
  */
 fun <T> List<Try<T>>.unwrapAll(): List<T> {
     return map { !!it }
+}
+
+/**
+ * Convert the given Try instance to
+ * a MvRx Async
+ */
+fun <T> Try<T>.toAsync(): Async<T> {
+    return when (this) {
+        is Try.Failure -> Fail(exception)
+        is Try.Success -> com.airbnb.mvrx.Success(value)
+    }
 }
