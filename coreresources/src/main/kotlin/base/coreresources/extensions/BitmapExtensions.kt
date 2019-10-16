c7e1da26739f5086cb0ed181fbe63f66061f59e1
Icaro Temponi <icarohs7@gmail.com>
2019-10-16 15:29:16 -0300

    Add resizeKeepingAspectRatio extension of Bitmap
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- coreresources/src/main/kotlin/base/coreresources/extensions/BitmapExtensions.kt
+++ coreresources/src/main/kotlin/base/coreresources/extensions/BitmapExtensions.kt
@@ -1,6 +1,7 @@
 package base.coreresources.extensions
 
 import android.graphics.Bitmap
+import kotlin.math.round
 
 /**
  * Split the given bitmap in parts, each not
@@ -26,4 +27,15 @@ fun Bitmap.splitIntoSequence(partMaxHeight: Int): Sequence<Bitmap> {
  */
 fun Bitmap.cutHeight(offset: Int, partHeight: Int): Bitmap {
     return Bitmap.createBitmap(this, 0, offset, width, partHeight)
+}
+
+/**
+ * Scale the given bitmap to a new size, keeping it's
+ * aspect ratio
+ * @param newWidth Width to be scaled to
+ */
+fun Bitmap.resizeKeepingAspectRatio(newWidth: Int): Bitmap {
+    val aspectRatio = width.toDouble() / height
+    val newHeight = round(newWidth / aspectRatio).toInt()
+    return Bitmap.createScaledBitmap(this, newWidth, newHeight, false)
 }
