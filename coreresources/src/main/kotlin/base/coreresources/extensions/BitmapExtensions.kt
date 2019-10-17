ff83273834554a195acb724c8909e4047430ff89
Icaro Temponi <icarohs7@gmail.com>
2019-10-17 08:55:52 -0300

    Add saveToContentProvider extension of Bitmap
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- coreresources/src/main/kotlin/base/coreresources/extensions/BitmapExtensions.kt
+++ coreresources/src/main/kotlin/base/coreresources/extensions/BitmapExtensions.kt
@@ -3,10 +3,10 @@ package base.coreresources.extensions
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.system.Os.close
+import android.net.Uri
 import androidx.core.content.FileProvider
 import arrow.core.Try
-import base.coreresources.R
+import com.github.icarohs7.unoxcore.extensions.orThrow
 import com.github.icarohs7.unoxcore.toplevel.tryBg
 import java.io.File
 import java.io.FileOutputStream
@@ -78,15 +78,7 @@ fun Bitmap.resizeKeepingAspectRatio(newWidth: Int): Bitmap {
  */
 suspend fun Bitmap.share(context: Context, providerUri: String, shareSheetTitle: String): Try<Unit> {
     return tryBg {
-        val imagesFolder = File(context.cacheDir, "images")
-        imagesFolder.mkdirs()
-        val file = File(imagesFolder, "shared_image.png")
-        FileOutputStream(file).apply {
-            compress(Bitmap.CompressFormat.PNG, 90, this)
-            flush()
-            close()
-        }
-        val uri = FileProvider.getUriForFile(context, providerUri, file)
+        val uri = saveToContentProvider(context, providerUri, "shared_image.png").orThrow()
         val intent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_STREAM, uri)
@@ -94,5 +86,48 @@ suspend fun Bitmap.share(context: Context, providerUri: String, shareSheetTitle:
             type = "image/*"
         }
         context.startActivity(Intent.createChooser(intent, shareSheetTitle))
+    }
+}
+
+/**
+ * Save the given Bitmap to a content provider using the
+ * given name for the file
+ * ### Need to add content provider to manifest to work:
+ * ```
+ * <provider
+ *     android:name="androidx.core.content.FileProvider"
+ *     android:authorities="com.mydomain.fileprovider"
+ *     android:exported="false"
+ *     android:grantUriPermissions="true">
+ *     <meta-data
+ *         android:name="android.support.FILE_PROVIDER_PATHS"
+ *         android:resource="@xml/file_paths" />
+ * </provider>
+ * ```
+ * ### Also, need to create the file file_paths.xml under the xml folder on res with the given content:
+ * ```
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <resources>
+ *     <paths>
+ *         <cache-path
+ *             name="shared_images"
+ *             path="images/" />
+ *     </paths>
+ * </resources>
+ * ```
+ * @param providerUri Uri used for the content provider, e.g com.mydomain.fileprovider
+ * @param fileName Name used by the saved file
+ */
+suspend fun Bitmap.saveToContentProvider(context: Context, providerUri: String, fileName: String): Try<Uri> {
+    return tryBg {
+        val imagesFolder = File(context.cacheDir, "images")
+        imagesFolder.mkdirs()
+        val file = File(imagesFolder, fileName)
+        FileOutputStream(file).apply {
+            compress(Bitmap.CompressFormat.PNG, 90, this)
+            flush()
+            close()
+        }
+        FileProvider.getUriForFile(context, providerUri, file)
     }
 }
