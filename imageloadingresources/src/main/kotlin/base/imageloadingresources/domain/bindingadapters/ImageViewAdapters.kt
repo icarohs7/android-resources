f05bf1a3387930f3cb9b36044840c2eee274605f
Icaro Temponi <icarohs7@gmail.com>
2019-10-23 13:21:53 -0300

    Remove image of View when passing null to loadUrlImage
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- imageloadingresources/src/main/kotlin/base/imageloadingresources/domain/bindingadapters/ImageViewAdapters.kt
+++ imageloadingresources/src/main/kotlin/base/imageloadingresources/domain/bindingadapters/ImageViewAdapters.kt
@@ -3,12 +3,17 @@ package base.imageloadingresources.domain.bindingadapters
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import base.imageloadingresources.domain.extensions.load
+import coil.api.clear
 
 /**
  * Binding adapter used to load an image
  * into an ImageView on xml
  */
 @BindingAdapter("app:url_src")
-fun ImageView.loadUrlImage(path: String) {
+fun ImageView.loadUrlImage(path: String?) {
+    if (path == null) {
+        clear()
+        return
+    }
     load(path)
 }
